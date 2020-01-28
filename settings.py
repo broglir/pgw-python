d8ab17683850b32da5da5d7ee8ef56567b944d1c
Roman Brogli <roman.brogli@env.ethz.ch>
2020-01-28 10:44:23 +0100

    added script to calculate the difference in specific humidity using relative humidity

--- settings.py
+++ settings.py
@@ -2,6 +2,32 @@
 
 import subprocess
 
+#should change in specific humidity be computed from relative humidity?
+calhumidity = True
+
+# settings for humidity routine. See documentation in humidity_difference_via_RH.py
+if calhumidity == True:
+	
+	humidity_hist_path = '/home/broglir/polybox/pgw-python/RELHUM_CT_Part.nc'
+	pressure_hist_path = '/home/broglir/polybox/pgw-python/P_CT_Part.nc'
+	temperature_hist_path =  '/home/broglir/polybox/pgw-python/T_CT_Part.nc'
+
+	humidity_future_path = '/home/broglir/polybox/pgw-python/RELHUM_SC_Part.nc'
+	pressure_future_path = '/home/broglir/polybox/pgw-python/P_SC_Part.nc'
+	temperature_future_path = '/home/broglir/polybox/pgw-python/T_SC_Part.nc'
+
+	savename = '/home/broglir/polybox/pgw-python/spec_hum_diff.nc'
+
+	humidityname='RELHUM'
+	pressurename='P'
+	temperaturename='T'
+	savevariablename = 'QV'
+
+	commandhum = f"python humidity_difference_via_RH.py {humidity_hist_path} {pressure_hist_path} {temperature_hist_path} {humidity_future_path} {pressure_future_path} {temperature_future_path} {savename} {humidityname} {pressurename} {temperaturename} {savevariablename} > outfile_calhum.txt &"
+	subprocess.run(commandhum, shell=True)
+	print(commandhum)
+
+
 performsmooth = False
 if performsmooth == True:
 #settings for timesmoothing script:
@@ -20,7 +46,7 @@ if performsmooth == True:
 	print(commandsmooth)
     
 
-performinterp = True
+performinterp = False
 if performinterp == True:
 	#see documentation in interpolate.py
 	filepathint = '/Users/roman/polybox/python_pgw/T_2M_flteredcycle.nc'
